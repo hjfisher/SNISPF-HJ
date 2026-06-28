9ca3630a23eed8cae8cedc6060df37017f881cc1
hjfisher <hojatollahmombeni@gmail.com>
2026-06-28 14:49:11 +0330

    Update __init__.py

--- sni_spoofing/bypass/__init__.py
+++ sni_spoofing/bypass/__init__.py
@@ -1,6 +1,7 @@
 """Bypass strategy implementations."""
 
 from .base import BypassStrategy
+from .direct import DirectBypass
 from .fragment import FragmentBypass
 from .fake_sni import FakeSNIBypass
 from .combined import CombinedBypass
@@ -8,6 +9,7 @@ from .raw_injector import RawInjector, is_raw_available
 
 __all__ = [
     "BypassStrategy",
+    "DirectBypass",
     "FragmentBypass",
     "FakeSNIBypass",
     "CombinedBypass",
